a5e73994b578a1d63ab6eb0145b1b79eddd95747
qiq <13383332752@163.com>
2019-01-20 13:56:13 +0800

    增加了top10

--- bbs/settings.py
+++ bbs/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '(9zkf1hvh)hjf*35i)4cgkgh(ek!7ujd%c!u%0&#vp@dblbwxi'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -65,7 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bbs.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -75,7 +72,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -95,7 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -108,7 +103,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
@@ -132,8 +126,12 @@ CACHES = {
     }
 }
 
-
-
+# Redis 配置
+REDIS = {
+    'host': 'localhost',
+    'port': 6379,
+    'db': 7
+}
 
 '''
 微博接入
@@ -141,10 +139,3 @@ APP_key = ''
 APP_SECRET = ''
 授权回调页
 '''
-
-
-
-
-
-
-
--- post/views.py
+++ post/views.py
@@ -3,8 +3,9 @@ from math import ceil
 from django.core.cache import cache
 from django.shortcuts import render, redirect
 
-from post.helper import page_cache
+from post.helper import page_cache, get_top_n
 from post.models import Post
+from common import rds
 
 
 def create_post(request):
@@ -46,18 +47,23 @@ def read_post(request):
 
     key = 'Post-%s' % post_id
     post = cache.get(key)
-    print('从缓存获取：',post)
+    print('从缓存获取：', post)
     if post is None:
         post = Post.objects.get(pk=post_id)
         cache.set(key, post)
-        print('从数据库获取：',post)
+        print('从数据库获取：', post)
+
+    # 增加阅读计数
+    rds.zincrby(name='ReadCounter', amount=post_id, value=0)
     return render(request, 'read_post.html', {'post': post})
 
 
 def delete_post(request):
     post_id = int(request.GET.get('post_id'))
     Post.objects.get(pk=post_id).delete()
+    rds.zrem('ReadCount', post_id)
     return redirect('/')
+
 
 @page_cache(5)
 def post_list(request):
@@ -70,10 +76,23 @@ def post_list(request):
     end = start + per_page
     posts = Post.objects.filter()[start:end]
 
-    return render(request, 'post_list.html', {'posts': posts,'pages':range(pages)})
+    return render(request, 'post_list.html', {'posts': posts, 'pages': range(pages)})
 
 
 def search(request):
     keyword = request.POST.get('keyword')
     posts = Post.objects.filter(title__contains=keyword)
     return render(request, 'search.html', {'posts': posts})
+
+
+def top10(request):
+    rank_data = [
+
+    ]
+    rank_data = get_top_n(10)
+    # rank_data = [[int(i), int(v)] for i, v in [item for item in rank_data]]
+    return render(request, 'top10.html', {'rank_data': rank_data})
+
+
+
+
